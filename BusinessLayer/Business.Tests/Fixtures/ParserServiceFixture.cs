499d4fbda7b13a341b147c5a0e741c3258a92785
Dmytro Zaverukha <dmytrozaverukha@gmail.com>
2019-09-01 03:11:34 +0300

    Update unit tests

--- BusinessLayer/Business.Tests/Fixtures/ParserServiceFixture.cs
+++ BusinessLayer/Business.Tests/Fixtures/ParserServiceFixture.cs
@@ -2,6 +2,7 @@
 {
     using Business.Services;
     using Moq;
+    using System.IO;
     using Xunit;
 
     public class ParserServiceFixture
@@ -26,11 +27,21 @@
                 "./TestFiles/logs_02.out",
                 "./TestFiles/logs_03.out"
             };
-            
+
+            int linesAmount = 0;
+
+            foreach (string file in mockFiles)
+            {
+                string[] lines = File.ReadAllLines(file);
+
+                // -1 from lines lenght is for remove first "Header" line
+                linesAmount += (lines.Length - 1);
+            }
+
             parserService.RunParser(mockFiles);
 
             Assert.True(ParallelForeachParserService.logModels.Count > 0);
-            Assert.Equal(300, ParallelForeachParserService.logModels.Count);
+            Assert.Equal(linesAmount, ParallelForeachParserService.logModels.Count);
         }
     }
 }
